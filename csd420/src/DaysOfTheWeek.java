0c672d7d4ecdb0a8646535a63cd833c157476f78
Scott Shannon <scott.a.shannon@outlook.com>
2022-06-22 22:05:09 -0700

    java enum example

---
+++ csd420/src/DaysOfTheWeek.java
@@ -0,0 +1,19 @@
+public enum DaysOfTheWeek {
+    SUNDAY(5),
+    MONDAY(1),
+    TUESDAY(3),
+    WEDNESDAY(5),
+    THURSDAY(7),
+    FRIDAY(10),
+    SATURDAY(10);
+    // Enum entries
+
+    final int levelOfHappiness;
+    // final so that a monday fanboy cannot change mondays levelOfHappiness to a 10
+
+    DaysOfTheWeek(int levelOfHappiness) {
+        // DaysOfTheWeek constructor
+        this.levelOfHappiness = levelOfHappiness;
+        // value passed
+    }
+}

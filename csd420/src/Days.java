aa0fd844c3b95780a8b9b8b075e168cab291a747
Scott Shannon <scott.a.shannon@outlook.com>
2022-07-12 23:23:39 -0700

    finish mod 9 mysql

--- csd420/src/Days.java
+++ csd420/src/Days.java
@@ -1,14 +1,14 @@
-public class Days {
-    public static void main(String[] args) {
-        //DaysOfTheWeek day = new DaysOfTheWeek();
-        // Because enums are intended to be a predefined set of values, you can not create new ones like this
+// public class Days {
+//     public static void main(String[] args) {
+//         //DaysOfTheWeek day = new DaysOfTheWeek();
+//         // Because enums are intended to be a predefined set of values, you can not create new ones like this
 
-        for (DaysOfTheWeek allDays : DaysOfTheWeek.values()) {
-            System.out.println(allDays);
-            //prints all the values for that enum using a for each loop
-        }
+//         for (DaysOfTheWeek allDays : DaysOfTheWeek.values()) {
+//             System.out.println(allDays);
+//             //prints all the values for that enum using a for each loop
+//         }
 
-        System.out.println("FRIDAYS level of happiness is : " + DaysOfTheWeek.FRIDAY.levelOfHappiness);
-        // get fridays level of happiness
-    }
-}
+//         System.out.println("FRIDAYS level of happiness is : " + DaysOfTheWeek.FRIDAY.levelOfHappiness);
+//         // get fridays level of happiness
+//     }
+// }
